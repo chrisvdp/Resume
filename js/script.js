b9a1899c1c9de2c1148da557060b66937a7df57b
Chris van der Ploeg <chris@chrisvdp.com>
2012-11-09 17:31:11 -0700

    Better google analytics
    Get the time for a one page site.

--- js/script.js
+++ js/script.js
@@ -3,7 +3,31 @@ $('#top-arrow').click(function (e) {
     e.preventDefault();
     $("html, body").animate({ scrollTop: 0 }, 500);
 });
+//better Google analytics info
+var googleAnalyics = function () {
+    var timeTracking,
+        theTime = 0,
+        interval = 10000;
+    function trackTime() {
+        _gaq.push(['_trackPageview','_timing', 'the time', theTime]);
+        theTime += interval;
+        //console.log(theTime);
+    };
+    function startTimeTracker() {
+        timeTracking = setInterval(trackTime, interval);
+    };
+    function stopTimeTracker() {
+        clearInterval(timeTracking);
+        theTime = 0;
+    };
+    $('#main-nav a').click(function () {
+        _gaq.push(['_trackEvent', 'Nav click', $(this).attr('href')]);
+    });
 
-$('#main-nav a').click(function () {
-    _gaq.push(['Nav', $(this).attr('href')]);
-});
+    return {
+        start: startTimeTracker,
+        stop: stopTimeTracker
+    };
+}();
+
+googleAnalyics.start();
